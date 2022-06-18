671ba57c2371e0bd986534db26f9177b6da3baf2
Alexis Boni <mail@aboni.dev>
2022-06-17 23:31:26 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# AstroNvim config
+
+My AsroNvim config mostly aimed at web development.
+Complementary blog post: https://blog.aboni.dev/2022-04-17-neovim-as-ide.md
---
+++ tests/react.jsx
@@ -0,0 +1,12 @@
+const react = () => {
+  return (
+    <div>
+      <h1>asdasd</h1>
+      <div>
+        <span></span>
+      </div>
+    </div>
+  );
+};
+
+export default react;
